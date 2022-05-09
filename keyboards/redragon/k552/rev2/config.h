f778e48365b60f862ee4ac85e8cf5438964091f5
Manuel Puentes <manuhank@gmail.com>
2022-05-09 14:32:28 -0300

    config k552 r2

--- keyboards/redragon/k552/rev2/config.h
+++ keyboards/redragon/k552/rev2/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x0C45
-#define PRODUCT_ID      0x5004
+#define VENDOR_ID       0x0267
+#define PRODUCT_ID      0x05AC
 #define DEVICE_VER      0x0001
 
 #define MANUFACTURER    Redragon
--- keyboards/redragon/k552/rev2/rules.mk
+++ keyboards/redragon/k552/rev2/rules.mk
@@ -25,6 +25,7 @@ SERIAL_LINK_ENABLE = no
 WAIT_FOR_USB = no
 CUSTOM_MATRIX = yes
 KEYBOARD_SHARED_EP = yes
+APPLE_FN_ENABLE = yes 
 
 # Custom RGB matrix handling
 RGB_MATRIX_ENABLE = yes
