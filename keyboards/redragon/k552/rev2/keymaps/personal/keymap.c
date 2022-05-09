ea70322272fba6fd8fdbe39c91ae47848696d67e
Manuel Puentes <manuhank@gmail.com>
2022-05-09 14:57:00 -0300

    fixed mm personal

--- keyboards/redragon/k552/rev2/keymaps/personal/keymap.c
+++ keyboards/redragon/k552/rev2/keymaps/personal/keymap.c
@@ -27,7 +27,7 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*  Row:        0           1           2           3        4           5        6        7           8           9           10          11          12          13          14          15          16          */
-    [_BASE] = { {   KC_ESC,     KC_MSEL,    KC_VOLD,    KC_VOLU, KC_MUTE,    KC_MSTP, KC_MPRV, KC_MPLY,    KC_MNXT,    KC_MAIL,    KC_WHOM,    KC_CALC,    KC_MCTL,    KC_NO,      KC_PSCR,    KC_SLCK,    KC_PAUSE },
+    [_BASE] = { {   KC_ESC,     KC_MSEL,    KC_VOLD,    KC_VOLU, KC_MUTE,    KC_MSTP, KC_MPRV, KC_MPLY,    KC_MNXT,    KC_MAIL,    KC_WHOM,    KC_CALC,    RGB_TOG,    KC_NO,      KC_PSCR,    KC_SLCK,    KC_PAUSE },
                 {   KC_GRV,     KC_1,       KC_2,       KC_3,    KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_INS,     KC_HOME,    KC_PGUP  },
                 {   KC_TAB,     KC_Q,       KC_W,       KC_E,    KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_NO,      KC_DEL,     KC_END,     KC_PGDN  },
                 {   KC_CAPS,    KC_A,       KC_S,       KC_D,    KC_F,       KC_G,    KC_H,    KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_NUHS,    KC_ENT,     KC_NO,      KC_NO,      KC_NO    },
