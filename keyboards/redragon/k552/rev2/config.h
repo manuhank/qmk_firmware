9e032f0f94c75e321b2ae64de2785f999495a8b8
Manuel Puentes <manuhank@gmail.com>
2022-05-10 07:36:29 -0300

    fixes

--- keyboards/redragon/k552/rev2/config.h
+++ keyboards/redragon/k552/rev2/config.h
@@ -19,8 +19,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x0267
-#define PRODUCT_ID      0x05AC
+#define VENDOR_ID       0x05AC
+#define PRODUCT_ID      0x0267
 #define DEVICE_VER      0x0001
 
 #define MANUFACTURER    Redragon
